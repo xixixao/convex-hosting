79062efd213405c61ddc094ed0e69810faff690c
Michal Srb <srb@convex.dev>
2023-08-28 02:43:42 +0200

    Use suspense for clientside transitions

--- app/hooks.ts
+++ app/hooks.ts
@@ -1,4 +1,4 @@
-import { useQuery as convexUseQuery } from "convex/react";
+import { useQuery as convexUseQuery, useConvex } from "convex/react";
 import {
   FunctionReference,
   OptionalRestArgs,
@@ -7,6 +7,7 @@ import {
 import { convexToJson } from "convex/values";
 import { useAsync } from "react-streaming";
 import { ActionCtx } from "../convex/_generated/server";
+import { useEffect, useMemo, useRef } from "react";
 
 export function useQuery<Query extends FunctionReference<"query">>(
   query: Query,
@@ -22,7 +23,37 @@ export function useQuery<Query extends FunctionReference<"query">>(
           )
         : undefined
   );
-  const live =
-    typeof window === "undefined" ? undefined : convexUseQuery(query, ...args);
-  return live ?? ssr;
+  if (typeof window === "undefined") {
+    return ssr;
+  }
+  const convex = useConvex();
+  const live = convexUseQuery(query, ...args);
+  const liveOrSsr = live === undefined ? ssr : live;
+  // if (promiseRef.current !== undefined && live !== undefined) {
+  //   promiseRef.current(live);
+  // }
+  if (liveOrSsr !== undefined) {
+    return liveOrSsr;
+  }
+
+  // Both live and ssr are undefined, this is a clientside
+  // transition, suspend!
+  let resume;
+  const watch = convex.watchQuery(query, ...args);
+  watch.onUpdate(() => {
+    resume!();
+  });
+
+  // TODO: This is caused by BS inside useQueries,
+  // `convexUseQuery` doesn't return the value on
+  // first render
+  const live_REMOVE = watch.localQueryResult();
+  if (live_REMOVE !== undefined) {
+    return live_REMOVE;
+  }
+  // END TODO
+
+  throw new Promise((resolve) => {
+    resume = resolve;
+  });
 }
